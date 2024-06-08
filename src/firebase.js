49974345a0ad8f0b80a3cec4490ebe8ec99fab19
Evans Yee <evans.yeecl@gmail.com>
2024-06-08 15:44:05 +0800

    changed settings for upload into vercel

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDQqaTp6yvjgNoiG2Wl9Zlv49D8Z8JweXs",
-    authDomain: "skivetracker-d97f7.firebaseapp.com",
-    projectId: "skivetracker-d97f7",
-    storageBucket: "skivetracker-d97f7.appspot.com",
-    messagingSenderId: "1046224209026",
-    appId: "1:1046224209026:web:d36564664dbb047c80209b",
-    measurementId: "G-HLWYBB3YHG"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
@@ -24,3 +24,12 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+
+// apiKey: "AIzaSyDQqaTp6yvjgNoiG2Wl9Zlv49D8Z8JweXs",
+// authDomain: "skivetracker-d97f7.firebaseapp.com",
+// projectId: "skivetracker-d97f7",
+// storageBucket: "skivetracker-d97f7.appspot.com",
+// messagingSenderId: "1046224209026",
+// appId: "1:1046224209026:web:d36564664dbb047c80209b",
+// measurementId: "G-HLWYBB3YHG"
